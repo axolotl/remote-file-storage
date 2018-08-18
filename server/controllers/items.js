ca5082f32a7f2de3131291088a677939aad8fbfe
Axolotl <samuelmachat@gmail.com>
2018-08-18 12:28:00 -0700

    api funcational for all database CRUD operations

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,32 +1,51 @@
 const Item = require('../models').Item
 
 module.exports = {
-  createFolder(req, res) {
-    return Item.create({
-      name: req.body.name,
-      type: 'folder',
-      belongsTo: req.body.belongsTo || 'base'
-    })
-      .then(score => res.status(201).send(score))
-      .catch(error => res.status(400).send(error))
+  create(req, res) {
+    if (req.body.type === 'folder') {
+      return Item.create({
+        name: req.body.name,
+        type: 'folder',
+        belongsTo: req.body.belongsTo || ''
+      })
+        .then(score => res.status(201).send(score))
+        .catch(error => res.status(400).send(error))
+    }
+    else if (req.body.type === 'file') {
+      return Item.create({
+        name: req.body.name,
+        type: 'file',
+        belongsTo: req.body.belongsTo || '',
+        location: req.body.location
+      })
+        .then(score => res.status(201).send(score))
+        .catch(error => res.status(400).send(error))
+    }
+    else {
+      return res.status(400).send('something went wrong')
+    }
   },
 
-  createFile(req, res) {
-    return Item.create({
-      name: req.body.name,
-      type: 'file',
-      belongsTo: req.body.belongsTo || 'base',
-      location: req.body.location
-    })
-      .then(score => res.status(201).send(score))
-      .catch(error => res.status(400).send(error))
+  rename(req, res) {
+    return Item.findById(req.params.itemId)
+      .then(item => {
+        if (!item) {
+          return res.status(400).send('item not found')
+        }
+        return item
+          .update({
+            name: req.body.name
+          })
+          .then(() => res.status(200).send(item))
+          .catch(error => res.status(400).send(error))
+      })
   },
 
   destroy(req, res) {
     return Item.findById(req.params.itemId)
       .then(item => {
         if (!item) {
-          return res.status(400).send('not found')
+          return res.status(400).send('item not found')
         }
         return item
           .destroy()
@@ -37,9 +56,10 @@ module.exports = {
   },
 
   list(req, res) {
-    return Item.findAll({ where: { belongsTo: req.params.folderId } })
+    return Item.findAll({
+      where: { belongsTo: req.params.folderId || '' }
+    })
       .then(scores => res.status(200).send(scores))
       .catch(error => res.status(400).send(error))
   }
-
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,33 @@ const itemsController = require('../controllers').items
 let mockData = require('../mockData/mockData')
 
 module.exports = app => {
-  app.get('/api/folders/:folderId', itemsController.list)
+  // app.get('/api/folders/', itemsController.list)
+  // app.get('/api/folders/:folderId', itemsController.list)
 
-  app.post('/api/files', itemsController.createFile)
-  app.post('/api/folders', itemsController.createFolder)
+  // app.post('/api/files', itemsController.createFile)
+  // app.post('/api/folders', itemsController.createFolder)
 
-  app.delete('/api/files/:fileId', itemsController.destroy)
-  app.delete('/api/folders/:folderId', itemsController.destroy)
+  // app.post('/api/files/:itemId', itemsController.rename)
+  // app.post('/api/folders/:itemId', itemsController.rename)
+
+  // app.delete('/api/files/:itemId', itemsController.destroy)
+  // app.delete('/api/folders/:itemId', itemsController.destroy)
+
+  // better routes
+
+  // get root level items (initial api call)
+  app.get('/api/items/', itemsController.list)
+
+  // get items nested inside folders
+  app.get('/api/items/:folderId', itemsController.list)
+
+  // create folders and files served from same route
+  // controller will each seperately based on type
+  app.post('/api/items', itemsController.create)
+
+  // rename items
+  app.post('/api/items/:itemId', itemsController.rename)
+
+  // delete items
+  app.delete('/api/items/:itemId', itemsController.destroy)
 }
