e1d3fd925c8286296f009be080f4b5e921f800a1
Axolotl <samuelmachat@gmail.com>
2019-04-07 23:01:08 -0400

    add testing route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,9 @@ const foldersRouter = require('./foldersRouter.js')
 module.exports = app => {
   app.use('/api/folders', foldersRouter)
   app.use('/api/files', filesRouter)
+
+  // testing route to check of server is live
+  app.get('/test', (req, res) => {
+    res.status(200).json({ message: 'server is live' })
+  })
 }
